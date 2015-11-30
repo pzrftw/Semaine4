2e531ee34cf150c441515f889041f1736326c1bc
Guillaume J <unknow123456@hotmail.fr>
2015-11-30 14:35:19 +0000

    Update Ex2

--- openfactfood.rb
+++ openfactfood.rb
@@ -1,1 +1,17 @@
 
+require 'open-uri'
+require 'json'
+
+loop do
+puts "Entrez un numero de code barre:"
+bar_code = gets.chomp
+
+url="http://fr.openfoodfacts.org/api/v0/produit/#{bar_code}.json"
+
+json = open(url).read
+
+data = JSON.parse(json)
+
+puts data['product']['product_name']['foodorigins']['product_origins']
+
+end
